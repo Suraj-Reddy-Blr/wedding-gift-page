62f39d4d602d180ba606fa3e39b31348099f037b
Suraj-Reddy-Blr <sure17mba@cmrit.ac.in>
2024-12-05 01:32:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.getElementById("login-form");
   const passwordInput = document.getElementById("password");
   const errorMessage = document.getElementById("error-message");
   const loginContainer = document.getElementById("login-container");
   const contentContainer = document.getElementById("content-container");
+  const giftImage = document.getElementById("gift-image");
+  const voucherCode = document.getElementById("voucher-code");
 
   loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -17,5 +20,10 @@ document.addEventListener("DOMContentLoaded", () => {
       errorMessage.style.display = "block";
       passwordInput.value = ""; // Clear the input field
     }
+  });
+
+  giftImage.addEventListener("click", () => {
+    voucherCode.style.display = "block";
+    giftImage.style.display = "none";
   });
 });
